3d2cc64acf3ced6369c03dec7c0e7a6e3ac90f71
freefireman <beluh.c.s@gmail.com>
2021-01-11 20:00:31 +0200

    little fix))

--- src/components/editUser-window/EditUserWindow.js
+++ src/components/editUser-window/EditUserWindow.js
@@ -2,37 +2,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { useReducer } from 'react';
 
 import { INPUT_NAME, INPUT_USERNAME, INPUT_EMAIL, INPUT_PHONE, INPUT_WEBSITE} from '../../redux/action-types';
-import {setNewName, setNewUsername, setNewEmail, setNewPhone, setNewWebsite} from '../../redux/action-creators';
+import {setNewName, setNewUsername, setNewEmail, setNewPhone, setNewWebsite,setID} from '../../redux/action-creators';
 
-export default function EditUserWindow({detailUserInfo, saveEditedUser}) {
+export default function EditUserWindow({detailUserInfo}) {
+    const {name, username, email, phone, website,id} = detailUserInfo;
+    const singleUser = useSelector(({singleUser_State: {singleUser}}) => singleUser);
+    const dispatch = useDispatch();
 
-    // const {detailUserInfo, saveEditedUser} = props;
-    const {name, username, email, phone, website} = detailUserInfo;
+    // dispatch(setID(id))
 
-// const {singleUser} = useSelector(({singleUser_State: {singleUser}}) => ({singleUser}))
-//     console.log(singleUser);
-// const dispatch = useDispatch();
+    const inputName = ({target: { value}}) => dispatch(setNewName(value));
+    const inputUsername = ({target: { value}}) => dispatch(setNewUsername(value));
+    const inputEmail = ({target: { value}}) => dispatch(setNewEmail(value));
+    const inputPhone = ({target: { value}}) => dispatch(setNewPhone(value));
+    const inputWebsite = ({target: { value}}) => dispatch(setNewWebsite(value));
 
 //todo зібрати юзіка тут і потім пхати у стор
 // todo неможна зробити це за допомогою 'redux', тільки {useReducer} from 'react'
 
-    const editedUserReducer = (state, action) => {
-        switch (action.type) {
-            case INPUT_NAME: return {...state, name: action.payload};
-            case INPUT_USERNAME: return {...state, username: action.payload};
-            case INPUT_EMAIL:return {...state, email: action.payload};
-            case INPUT_PHONE: return {...state, phone: action.payload};
-            case INPUT_WEBSITE: return {...state, website: action.payload};
-            default: return state;
-        }
-    };
-    const [editedUser, dispatchEditedUser] = useReducer(editedUserReducer, detailUserInfo);
-
-    const inputName = (e) => dispatchEditedUser(setNewName(e.target.value));
-    const inputUsername = (e) => dispatchEditedUser(setNewUsername(e.target.value));
-    const inputEmail = (e) => dispatchEditedUser(setNewEmail(e.target.value));
-    const inputPhone = (e) => dispatchEditedUser(setNewPhone(e.target.value));
-    const inputWebsite = (e) => dispatchEditedUser(setNewWebsite(e.target.value));
+    // const editedUserReducer = (state, action) => {
+    //     switch (action.type) {
+    //         case INPUT_NAME: return {...state, name: action.payload};
+    //         case INPUT_USERNAME: return {...state, username: action.payload};
+    //         case INPUT_EMAIL:return {...state, email: action.payload};
+    //         case INPUT_PHONE: return {...state, phone: action.payload};
+    //         case INPUT_WEBSITE: return {...state, website: action.payload};
+    //         default: return state;
+    //     }
+    // };
+    // const [editedUser, dispatchEditedUser] = useReducer(editedUserReducer, detailUserInfo);
+    //
+    // const inputName = (e) => dispatchEditedUser(setNewName(e.target.value));
+    // const inputUsername = (e) => dispatchEditedUser(setNewUsername(e.target.value));
+    // const inputEmail = (e) => dispatchEditedUser(setNewEmail(e.target.value));
+    // const inputPhone = (e) => dispatchEditedUser(setNewPhone(e.target.value));
+    // const inputWebsite = (e) => dispatchEditedUser(setNewWebsite(e.target.value));\
 
     return (
         <div>
@@ -58,8 +62,10 @@ export default function EditUserWindow({detailUserInfo, saveEditedUser}) {
                 <input onInput={inputWebsite} type="text" defaultValue={website}/>
             </div>
             <div>
-                <button onClick={() => saveEditedUser('save', editedUser)}>Save</button>
-                <button onClick={saveEditedUser}>Cancel</button>
+                <button onClick={() => {
+                    dispatch({type: 'UPDATE_USER', payload: {...singleUser, id: detailUserInfo.id}})
+                }}>Save</button>
+                {/*<button onClick={saveEditedUser}>Cancel</button>*/}
             </div>
         </div>
     )
